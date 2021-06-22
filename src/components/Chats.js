44dae114afdc427b69f8d8b5eb7a71aadca45a94
Steven Tejeda <steventejeda@gmail.com>
2021-06-21 20:09:13 -0400

    Custom user api

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import { ChatEngine } from 'react-chat-engine';
 import { auth } from '../firebase';
+import { useAuth } from '../contexts/AuthContext';
+import axios from 'axios';
 
 const Chats = () => {
   const history = useHistory();
+  const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
 
   const handleLogout = async () => { 
     await auth.signOut();
@@ -12,6 +16,50 @@ const Chats = () => {
     history.push('/');
   }
 
+  const getFile = async (url) => { 
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], "userPhoto.jpg", { type: 'image/jpeg' })
+  }
+
+  useEffect(() => { 
+    if(!user || user === null) { 
+      history.push('/')
+      return;
+    } 
+    axios.get('https://api.chatengine.io/users/me/', {
+      headers: {
+        "project-id": process.env.REACT_APP_CHAT_ENGINE_ID,
+        "user-name": user.email,
+        "user-secret": user.uid,
+      }
+    })
+      .then(() => { 
+        setLoading(false);
+      })
+      .catch(() => { 
+        let formdata = new FormData();
+        formdata.append('email', user.email);
+        formdata.append('username', user.email);
+        formdata.append('secret', user.uid);
+
+        getFile(user.photoURL)
+        .then((avatar) => {
+          formdata.append('avatar', avatar, avatar.name);
+
+          axios.post('https://api.chatengine.io/users/',
+            formdata,
+            { headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY}}
+            )
+            .then(() => setLoading(false))
+            .catch((error) => console.log(error))
+        })
+      })
+
+  }, [user, history]) 
+
+ if(!user || loading) return 'Loading...';
 
     return (
       <div className="chats-page">
@@ -25,10 +73,9 @@ const Chats = () => {
         </div>
         <ChatEngine 
           height="calc(100vh - 66px)"
-          projectId="
-          b93d7b2d-135f-48d7-8104-35146f7b9e01"
-          userName="."
-          userSecret="."
+          projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
+          userName={user.email}
+          userSecret={user.uid}
         />
       </div>
     )
